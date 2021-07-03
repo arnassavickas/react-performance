feab7adeec17c62192dc78e960cbe5a5ab021c58
Arnas Savickas <arnas.savi@gmail.com>
2021-07-03 20:42:53 +0300

    Finished 01 extra

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,40 +4,41 @@
 import * as React from 'react'
 
 const importGlobe = () => {
-  import('../globe')
+  return import(/* webpackPrefetch: true */ '../globe')
 }
+
 const Globe = React.lazy(importGlobe)
 
 function App() {
   const [showGlobe, setShowGlobe] = React.useState(false)
 
   return (
-    <React.Suspense fallback={<div>loading...</div>}>
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          height: '100%',
-          padding: '2rem',
-        }}
-      >
-        <label style={{marginBottom: '1rem'}}>
-          <input
-            type="checkbox"
-            checked={showGlobe}
-            onChange={e => setShowGlobe(e.target.checked)}
-            onMouseOver={importGlobe}
-            onFocus={importGlobe}
-          />
-          {' show globe'}
-        </label>
-        <div style={{width: 400, height: 400}}>
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        height: '100%',
+        padding: '2rem',
+      }}
+    >
+      <label style={{marginBottom: '1rem'}}>
+        <input
+          type="checkbox"
+          checked={showGlobe}
+          onChange={e => setShowGlobe(e.target.checked)}
+          onMouseOver={importGlobe}
+          onFocus={importGlobe}
+        />
+        {' show globe'}
+      </label>
+      <div style={{width: 400, height: 400}}>
+        <React.Suspense fallback={<div>loading...</div>}>
           {showGlobe ? <Globe /> : null}
-        </div>
+        </React.Suspense>
       </div>
-    </React.Suspense>
+    </div>
   )
 }
 
